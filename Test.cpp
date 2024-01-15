fadf0140e9be893557ff8a6fc53a74f66686c09f
KhiemNguyen <nphucanhduong@gmail.com>
2024-01-15 10:34:08 +0700

    Update Test.cpp ver 2.0

--- Test.cpp
+++ Test.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-  cout << "Hello Hell!";
+  if (argc != 2) {
+    cerr << "Error: Expected \"Testcpp <argument0>\"\n";
+    exit(1);
+  }
+  int term = std::stoi(argv[1]);
+  cout << "Hello Hell!\n";
+  int full = term*2 - 1;
+  for (int i = 1; i < term; ++i) {
+    int space = full - i*2 + 1;
+    for (int j = 0; j < space/2; ++j) cout << " ";
+    for (int j = 0; j < i*2 - 1; ++j) cout << "*";
+    for (int j = 0; j < space/2; ++j) cout << " ";
+    cout << endl;
+  }
   
   return 0;
 }
